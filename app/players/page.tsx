e5305dbaee8e13737eba3ba09137854ef463f9a3
Mohd Rashidee Jamaludin <rashidee@thestar.com.my>
2024-09-03 09:58:50 +0800

    filter player for only available players

--- app/players/page.tsx
+++ app/players/page.tsx
@@ -11,7 +11,36 @@ async function getData(): Promise<Player[]> {
 	);
 	let allData = await data.json();
 	let players = allData.elements;
-	return players;
+
+	let filteredPlayers = players.filter(function (player: {
+		goals_scored: any;
+		assists: any;
+		status: any;
+		expected_goal_involvements: number;
+		id: number;
+	}) {
+		const goalsassist = player.goals_scored + player.assists;
+		const difference = goalsassist - player.expected_goal_involvements;
+
+		return (
+			// costperxg > 0.5 &&
+			player.status == "a"
+			// difference > 0 &&
+			// player.now_cost < 40 &&
+			// player.points_per_game > 3 &&
+			// player.id == 524 || //Areola
+
+			// || player.id == 1
+			// player.element_type == 4
+			// &&
+			// (player.team == 1 ||
+			// 	player.team == 2 ||
+			// 	player.team == 13 ||
+			// 	player.team == 15)
+		);
+		// return difference > 0 && player.minutes > 230 && player.now_cost <= 50;
+	});
+	return filteredPlayers;
 }
 export default async function DemoPage() {
 	const data = await getData();
